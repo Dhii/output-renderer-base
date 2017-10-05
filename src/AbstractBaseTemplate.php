33b6d3be0f1be17ad36977835461c07549d5ded2
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-05 17:46:08 +0200

    Added `AbstractBaseTemplate`

---
+++ src/AbstractBaseTemplate.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Dhii\Output;
+
+use Exception as RootException;
+use Dhii\Output\Exception\RendererException;
+use Dhii\Output\Exception\TemplateRenderException;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Dhii\I18n\StringTranslatingTrait;
+use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
+
+/**
+ * Base concrete functionality for blocks.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractBaseTemplate extends AbstractTemplate implements TemplateInterface
+{
+    /*
+     * Adds internal i18n capabilities.
+     *
+     * @since [*next-version*]
+     */
+    use StringTranslatingTrait;
+
+    /*
+     * Adds internal factory for creating invalid argument exceptions.
+     *
+     * @since [*next-version*]
+     */
+    use CreateInvalidArgumentExceptionCapableTrait;
+
+    /**
+     * Creates a new render-related exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     *
+     * @return RendererException The new exception.
+     */
+    protected function _createRendererException(
+        $message = null,
+        $code = null,
+        RootException $previous = null
+    ) {
+        return new RendererException($message, $code, $previous, $this);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     *
+     * @return TemplateRenderException The new exception.
+     */
+    protected function _createTemplateException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $container = null
+    ) {
+        return new TemplateRenderException($message, $code, $previous, $this, $container);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function render($context = null)
+    {
+        return $this->_render($context);
+    }
+}
